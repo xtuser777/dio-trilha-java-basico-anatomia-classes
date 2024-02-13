a90d8a920045279c3ea423204b4c528c57220a79
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-13 20:48:24 -0300

    aula3

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@ public class Main {
     
     public static void main(String[] args) {
         //System.out.println("Anatomia das classes");
-        Integer number = 1;
-        final String BR = "Brasil";
-        String variableName = "Name";
-        Double num123$23_1 = 1234.567;
+        // Integer number = 1;
+        // final String BR = "Brasil";
+        // String variableName = "Name";
+        // Double num123$23_1 = 1234.567;
+        String firstName = "Lucas";
+        String lastName = "Oliveira";
+
+        String completeName = completeName(firstName, lastName);
+        System.out.println(completeName);
+    }
+
+    public static String completeName(String firstName, String lastName) {
+        return firstName.concat(" ").concat(lastName);
     }
 }
