75de883d1189f5dd1b825465cd4bfbd775b78725
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-13 11:49:23 -0300

    aula2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Anatomia das classes");
+        //System.out.println("Anatomia das classes");
+        Integer number = 1;
+        final String BR = "Brasil";
+        String variableName = "Name";
+        Double num123$23_1 = 1234.567;
     }
 }
